894866928ed39cc9917224b22485d69055f34a06
Glitch (thunder-java) <none>
2019-09-26 11:45:49 +0000

    New stuff

---
+++ src/main/java/com/sharif/thunder/commands/utilities/UptimeCommand.java
@@ -0,0 +1,40 @@
+package com.sharif.thunder.commands.utilities;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.List;
+
+public class UptimeCommand extends Command {
+ 
+  public UptimeCommand() {
+    this.name = "uptime";
+    this.help = "Displays the amount of time that the bot has been up.";
+  } 
+  
+  @Override
+  protected void execute(CommandEvent event) {
+    final long duration = ManagementFactory.getRuntimeMXBean().getUptime();
+    final long years = duration / 31104000000L;
+    final long months = duration / 2592000000L % 12;
+    final long days = duration / 86400000L % 30;
+    final long hours = duration / 3600000L % 24;
+    final long minutes = duration / 60000L % 60;
+    final long seconds = duration / 1000L % 60;
+    final long milliseconds = duration % 1000;
+    
+    String uptime = (years == 0 ? "" : "**" + years + "** Years, ") + (months == 0 ? "" : "**" + months + "** Months, ") + (days == 0 ? "" : "**" + days + "** Days, ") + (hours == 0 ? "" : "**" + hours + "** Hours, ")
+      + (minutes == 0 ? "" : "**" + minutes + "** Minutes, ") + (seconds == 0 ? "" : "**" + seconds + "** Seconds, ") + (milliseconds == 0 ? "" : "**" + milliseconds + "** Milliseconds, ");
+    
+    uptime = replaceLast(uptime, ", ", "");
+    uptime = replaceLast(uptime, ",", " and");
+    
+    event.reply("I've been online for:\n" + uptime);
+  }
+  
+  private String replaceLast(final String text, final String regex, final String replacement) {
+    return text.replaceFirst("(?s)(.*)" + regex, "$1" + replacement);
+  }
+} 
